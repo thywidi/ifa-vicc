36bed463cc8ed2806efce32b861f2b51731359e8
thywidi <thierry.widmer@gmail.com>
2023-08-23 22:56:46 +0200

    Setup compontents

--- app/init.py
+++ app/init.py
@@ -6,17 +6,24 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/index")
 def home():
-    user = {"bla": "TTT", "age": 42, "home": "Bordeaux"}
+    user = {"bla": "Thierry", "age": 42, "home": "Bordeaux"}
     parkingSpots = {"1": "Free", "2": "Free", "3": "Free", "4": "Free"}
     income = 340
     return render_template(
-        "index.html", title="Home", user=user, income=income, parkingSpots=parkingSpots
+        "pages/index.html",
+        title="Home",
+        user=user,
+        income=income,
+        parkingSpots=parkingSpots,
     )
 
 
 @app.route("/login")
 def login():
-    return "Login now"
+    return render_template(
+        "pages/login.html",
+        title="Login",
+    )
 
 
 @app.route("/api")
